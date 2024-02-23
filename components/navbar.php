fc774867c6d8f51eb397e8ed12b4840a91c3146e
Bombay <OMGmsebastian@gmail.com>
2024-02-23 17:50:21 -0500

    fix(navbar): change to header tag

--- components/navbar.php
+++ components/navbar.php
@@ -17,13 +17,13 @@
   <?php include $root.'/components/styles/navbar.css'; ?>
 </style>
 
-<nav>
+<header>
   <a href="../">
     <img
         style="<?php echo !$blackLogo ? 'filter: invert(1);' : ''; ?>"
         class="logo" src="<?php echo $root.'/static/images/black_empty.png'; ?>" alt="Trailblazers logo" />
   </a>
-  <div class="nav__links">
+  <nav class="nav__links">
     <?php foreach($routes as $name => $path): ?>
       <a class="nav__link"
          style="<?php echo !$blackLogo ? 'color: var(--text-white);' : 'color: var(--text);'; ?>"
@@ -36,5 +36,5 @@
         <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
       </svg>
     </a>
-  </div>
-</nav>
+  </nav>
+</header>
